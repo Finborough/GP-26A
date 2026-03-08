eea3e684b63fca2c4ff3f9f778ef3baabcabaaba
Finborough <113507556+Finborough@users.noreply.github.com>
2026-03-08 21:13:16 +0200

    random useless stuff

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExampleClass : MonoBehaviour {
+public class Player : MonoBehaviour {
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,13 +1,31 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Player player;
+
+
+    public int[] numbers = new int[10]; 
+    public int numberObjectCount = 0;
+   
     void Start()
     {
-        TestClass testclass = new TestClass();
-        print(testclass.punaset);
+        int currentNumber = 0;
+        //  Loop through Array
+        foreach (int number in numbers){
+            // If fewer GameObjects than items in Array, break loop
+            if (GameObject.Find(currentNumber.ToString()) == null)
+            {
+                break;
+            }
+        // If GameObject matches number, print the found GameObject
+        print(GameObject.Find(currentNumber.ToString()));
+        currentNumber += 1;
+        numberObjectCount += 1;
+        }
+        print(numberObjectCount.ToString() + " number objects");
     }
 
     // Update is called once per frame
