befe6bcb9c3cf7c0f021d84fce9c075f2790474d
Finborough <113507556+Finborough@users.noreply.github.com>
2026-03-08 12:22:36 +0200

    Adding a 3D Tilemap, lighting, pixelated effect

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,55 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
-{
+public class ExampleClass : MonoBehaviour {
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
 
-
-    public float speed = 7.5f;
-    public float gravity = 20.0f;
     public Camera camera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
-
-    public CharacterController controller;
-
-    CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
-    public bool canMove = true;
-    public float movementSpeed;
-
     void Start()
     {
-
-        characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
-
-    void Update()
-    {
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
-
-        if (!characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            //if (Input.GetButton("Jump"))
+            //    moveDirection.y = jumpSpeed;
+            
         }
-
-        characterController.Move(moveDirection * Time.deltaTime);
-    
-        if (canMove)
-        {
-            rotationX += Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            camera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+ 
+        rotationX -= Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        camera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
-
-        
-
     }
-}
+
+
+
+
 
 
